381538d7a768e237f2a115899687fcd351a77133
Mudassir Riaz <riazmudassir64@gmail.com>
2024-02-23 08:19:38 +0500

    Lab Tasks Done

--- OOP SECOND LAB/TASKS.cpp
+++ OOP SECOND LAB/TASKS.cpp
@@ -1,34 +1,103 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class livingthings // Parent 1 class
-{
-	void  breathe()
-	{
-		cout<<"Living things are breathing"<<endl;
-	}
-};
-
-class animal :public livingthings // Parent 2 class
+class Animal
 {
 public:
-	void walk()
-	{
-		cout << "The Animals are walking" << endl;
-	}
+    int age;
+    Animal(int age) : age(age) {}
+    virtual void makeSound() const = 0;
+    virtual void eat() const = 0;
 };
-class dog :public animal // Child class
+
+class Lion : public Animal
 {
-	public:
-	void bark()
-	{
-		cout<<"The Dog Barks"<<endl;
-	}
+public:
+    Lion(int age) : Animal(age) {}
+    void makeSound() const override
+    {
+        cout << "Lion is " << age << " years old\n";
+        cout << "Lion Says Roar!\n";
+    }
+    void eat() const override
+    {
+        cout << "Lion is eating meat.\n";
+    }
 };
+
+class Elephant : public Animal
+{
+public:
+    Elephant(int age) : Animal(age) {}
+    void makeSound() const override
+    {
+        cout << "Elephant is " << age << " years old\n";
+        cout << "Elephant Trumpet!\n";
+    }
+    void eat() const override
+    {
+        cout << "Elephant is eating grass.\n";
+    }
+};
+
+class Monkey : public Animal
+{
+public:
+    Monkey(int age) : Animal(age) {}
+    void makeSound() const override
+    {
+        cout << "Monkey is " << age << " years old\n";
+        cout << "Monkey Says Ooh ooh, eee eee!\n";
+    }
+    void eat() const override
+    {
+        cout << "Monkey is eating bananas.\n";
+    }
+};
+
+class Bird : public Animal
+{
+public:
+    Bird(int age) : Animal(age) {}
+    void makeSound() const override
+    {
+        cout << "Bird is " << age << " years old\n";
+        cout << "Bird Says Chirp!\n";
+    }
+    void eat() const override
+    {
+        cout << "Bird is eating seeds.\n";
+    }
+};
+
+void hearSound(const Animal& animal)
+{
+    animal.makeSound();
+}
+
+void seeEating(const Animal& animal)
+{
+    animal.eat();
+}
 
 int main()
 {
-	dog mydog;
-	mydog.walk();
-	mydog.bark();
+    Lion lion(5);
+    Elephant elephant(10);
+    Monkey monkey(3);
+    Bird bird(2);
+
+    hearSound(lion);
+    seeEating(lion);
+    cout<<"***********************\n";
+    hearSound(elephant);
+    seeEating(elephant);
+    cout << "***********************\n";
+    hearSound(monkey);
+    seeEating(monkey);
+    cout << "***********************\n";
+    hearSound(bird);
+    seeEating(bird);
+    cout << "***********************\n";
+    return 0;
 }
